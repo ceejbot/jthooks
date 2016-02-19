1b66ef6068ef3bfadd299e31a5102a673c0eed98
C J Silverio <ceejceej@gmail.com>
2016-02-18 17:52:41 -0800

    Update dependencies. Add linting.

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 
 var
-    _      = require('lodash'),
-    assert = require('assert'),
-    Github = require('github')
-    ;
+	isObject = require('lodash.isobject'),
+	assert   = require('assert'),
+	Github   = require('github')
+	;
 
 var Jthooks = module.exports = function Jthooks(opts)
 {
-    assert(opts && _.isObject(opts), 'you must pass an options object to the constructor');
-    assert(opts.auth && _.isObject(opts.auth), 'you must pass an `auth` object option');
-    this.options = opts;
+	assert(opts && isObject(opts), 'you must pass an options object to the constructor');
+	assert(opts.auth && isObject(opts.auth), 'you must pass an `auth` object option');
+	this.options = opts;
 
-    this.client = new Github(
-    {
-        version: "3.0.0",
-        timeout: 5000
-    });
+	this.client = new Github({
+		version: '3.0.0',
+		timeout: 5000
+	});
 };
 
 Jthooks.prototype.options = null;
@@ -23,99 +22,95 @@ Jthooks.prototype.client = null;
 
 Jthooks.prototype.authenticate  = function()
 {
-    this.client.authenticate(this.options.auth);
+	this.client.authenticate(this.options.auth);
 };
 
 Jthooks.prototype.setWebhook = function(opts, callback)
 {
-    var self = this;
-    var func = (opts.id ? 'hook' : 'exists');
+	var self = this;
+	var func = (opts.id ? 'hook' : 'exists');
 
-    self[func](opts, function(err, hook)
-    {
-        if (err) return callback(err);
-        if (hook)
-            return self.update(hook, opts, callback);
-        self.create(opts, callback);
-    })
+	self[func](opts, function(err, hook)
+	{
+		if (err) return callback(err);
+		if (hook)
+			return self.update(hook, opts, callback);
+		self.create(opts, callback);
+	});
 };
 
 Jthooks.prototype.update = function(hook, opts, callback)
 {
-    var newHook =
-    {
-        id:     hook.id,
-        repo:   opts.repo,
-        user:   opts.user,
-        name:   hook.name,
-        active: ('active' in opts ? opts.active : true),
-        events: ('events' in opts ? opts.events : [ 'push' ]),
-        config:
-        {
-            content_type: 'json',
-            secret: opts.secret,
-            url: opts.url
-        },
-    };
+	var newHook = {
+		id:     hook.id,
+		repo:   opts.repo,
+		user:   opts.user,
+		name:   hook.name,
+		active: ('active' in opts ? opts.active : true),
+		events: ('events' in opts ? opts.events : [ 'push' ]),
+		config: {
+			content_type: 'json',
+			secret: opts.secret,
+			url: opts.url
+		},
+	};
 
-    this.authenticate();
-    this.client.repos.updateHook(newHook, function(err, result)
-    {
-        callback(err, false, result);
-    });
+	this.authenticate();
+	this.client.repos.updateHook(newHook, function(err, result)
+	{
+		callback(err, false, result);
+	});
 };
 
 Jthooks.prototype.create  = function(opts, callback)
 {
-    var hookOpts =
-    {
-        repo: opts.repo,
-        user: opts.user,
-        name:   'web',
-        active: true,
-        events: [ 'push' ],
-        config:
-        {
-            content_type: 'json',
-            secret: opts.secret,
-            url: opts.url
-        },
-    };
+	var hookOpts = {
+		repo: opts.repo,
+		user: opts.user,
+		name:   'web',
+		active: true,
+		events: [ 'push' ],
+		config: {
+			content_type: 'json',
+			secret: opts.secret,
+			url: opts.url
+		},
+	};
 
-    this.authenticate();
-    this.client.repos.createHook(hookOpts, function(err, result)
-    {
-        callback(err, true, result);
-    });
+	this.authenticate();
+	this.client.repos.createHook(hookOpts, function(err, result)
+	{
+		callback(err, true, result);
+	});
 };
 
 Jthooks.prototype.exists = function(opts, callback)
 {
-    var self = this;
+	var self = this;
 
-    self.hooks(opts, function(err, hooks)
-    {
-        if (err) return callback(err);
-        for (var i = 0; i < hooks.length; i++)
-        {
-            var hook = hooks[i];
-            if (hook.config.url === opts.url)
-                return callback(null, hook);
-            // that check might need to be looser
-        }
+	self.hooks(opts, function(err, hooks)
+	{
+		if (err) return callback(err);
+		for (var i = 0; i < hooks.length; i++)
+		{
+			var hook = hooks[i];
+			if (hook.config.url === opts.url)
+				return callback(null, hook);
+			// that check might need to be looser
+		}
 
-        callback(null, false); // not found
-    });
+		callback(null, false); // not found
+	});
 };
 
 Jthooks.prototype.hook = function(opts, callback)
 {
-    this.authenticate();
-    this.client.repos.getHook(opts, callback);
+	this.authenticate();
+	this.client.repos.getHook(opts, callback);
 };
 
 Jthooks.prototype.hooks = function(opts, callback)
 {
-    this.authenticate();
-    this.client.repos.getHooks(opts, callback);
+	this.authenticate();
+	this.client.repos.getHooks(opts, callback);
 };
--- test/test-01-jthooks.js
+++ test/test-01-jthooks.js
@@ -1,56 +1,51 @@
+/*global describe:true, it:true, before:true, after:true */
 'use strict';
 
 var
-    demand   = require('must'),
-    sinon    = require('sinon'),
-    Jthooks  = require('../index')
-    ;
+	demand   = require('must'),
+	sinon    = require('sinon'),
+	Jthooks  = require('../index')
+	;
 
 describe('jthooks', function()
 {
-    var goodOpts =
-    {
-        auth: { type: "oauth", token: 'deadbeef' }
-    };
+	var goodOpts = { auth: { type: 'oauth', token: 'deadbeef' } };
 
-    it('exports a single function', function(done)
-    {
-        Jthooks.must.be.a.function();
-        done();
-    });
+	it('exports a single function', function(done)
+	{
+		Jthooks.must.be.a.function();
+		done();
+	});
 
-    it('requires an options object', function(done)
-    {
-        function shouldThrow() { return new Jthooks(); }
-        shouldThrow.must.throw(/options/);
-        done();
-    });
+	it('requires an options object', function(done)
+	{
+		function shouldThrow() { return new Jthooks(); }
+		shouldThrow.must.throw(/options/);
+		done();
+	});
 
-    it('requires an auth option', function(done)
-    {
-        function shouldThrow() { return new Jthooks({}); }
-        shouldThrow.must.throw(/auth/);
-        done();
-    });
+	it('requires an auth option', function(done)
+	{
+		function shouldThrow() { return new Jthooks({}); }
+		shouldThrow.must.throw(/auth/);
+		done();
+	});
 
-    it('can be constructed', function(done)
-    {
-        var j = new Jthooks(goodOpts);
-        j.must.be.instanceof(Jthooks);
-        j.options.must.eql(goodOpts);
-        done();
-    });
+	it('can be constructed', function(done)
+	{
+		var j = new Jthooks(goodOpts);
+		j.must.be.instanceof(Jthooks);
+		j.options.must.eql(goodOpts);
+		done();
+	});
 
-    it('authenticate() calls authenticate on the client', function(done)
-    {
-        var j = new Jthooks(goodOpts);
-        j.client.authenticate = sinon.spy();
-        j.authenticate();
-        j.client.authenticate.called.must.be.true();
-        j.client.authenticate.calledWith(goodOpts.auth).must.be.true();
-        done();
-    });
-
-
-
+	it('authenticate() calls authenticate on the client', function(done)
+	{
+		var j = new Jthooks(goodOpts);
+		j.client.authenticate = sinon.spy();
+		j.authenticate();
+		j.client.authenticate.called.must.be.true();
+		j.client.authenticate.calledWith(goodOpts.auth).must.be.true();
+		done();
+	});
 });
